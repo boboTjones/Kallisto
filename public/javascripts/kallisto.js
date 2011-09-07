1bd6f253b70f2ab22349e569552e6bdf26f86cbd
Erin Ptacek <erin@matasano.com>
2011-09-06 21:39:48 -0500

    I wanted to see if I could write a calendar view and then number the days with a jquery JSON request.

--- public/javascripts/kallisto.js
+++ public/javascripts/kallisto.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
     $('nav li').removeClass('active');
     var activeTab = $('#nav').attr('class');
     $('li.' + activeTab).addClass('active');
+
+    $.getJSON('calendar/dow', function(data) {
+      for ( x in data ) {
+        var dow = x;
+        var loc = data[x];
+        $("td:eq(" + loc + ")").find("label").append(dow);
+      }
+      });
+    
+
 });
